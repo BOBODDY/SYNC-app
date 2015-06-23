9e80e022ecca939c3d1829538ac34218ba7a9524
Nick Mathews <nick@codemettle.com>
2015-06-23 18:32:18 -0400

    cleaned up fragments

--- app/src/main/java/com/sync/syncapp/fragments/DashboardFragment.java
+++ app/src/main/java/com/sync/syncapp/fragments/DashboardFragment.java
@@ -15,18 +15,13 @@ import com.sync.syncapp.util.UserHandler;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DashboardFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link DashboardFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class DashboardFragment extends Fragment {
-//    private static final String ARG_USER_ID = "userId";
 
     private String userId;
 
-    private OnFragmentInteractionListener mListener;
 
     UserHandler userHandler;
 
@@ -40,15 +35,10 @@ public class DashboardFragment extends Fragment {
      */
     public static DashboardFragment newInstance() {
         DashboardFragment fragment = new DashboardFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_USER_ID, userId);
-//        fragment.setArguments(args);
         return fragment;
     }
 
-    public DashboardFragment() {
-        // Required empty public constructor
-    }
+    public DashboardFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,7 +49,8 @@ public class DashboardFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
@@ -71,45 +62,6 @@ public class DashboardFragment extends Fragment {
         //TODO: load the user's dashboard from the back end
         welcomeText = (TextView) getActivity().findViewById(R.id.dashboard_fragment_welcome);
         welcomeText.setText("Welcome, " + userId);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 
 }
