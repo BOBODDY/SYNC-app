cddbb855970ff5c615fd0db3c01d95ebead905aa
Nick Mathews <nick@codemettle.com>
2015-06-23 16:46:28 -0400

    created fragments package

--- app/src/main/java/com/sync/syncapp/fragments/DashboardFragment.java
+++ app/src/main/java/com/sync/syncapp/fragments/DashboardFragment.java
@@ -1,4 +1,4 @@
-package com.sync.syncapp;
+package com.sync.syncapp.fragments;
 
 import android.app.Activity;
 import android.net.Uri;
@@ -7,6 +7,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.sync.syncapp.R;
 
 
 /**
