f10d39bf470d40fe5bbf5793fa70eabe9b93a94e
Nick Mathews <nick@codemettle.com>
2015-07-13 13:55:46 -0400

    added layout items

--- app/src/main/java/com/sync/syncapp/fragments/StatisticsFragment.java
+++ app/src/main/java/com/sync/syncapp/fragments/StatisticsFragment.java
@@ -1,11 +1,14 @@
 package com.sync.syncapp.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
+import com.github.mikephil.charting.charts.LineChart;
 import com.sync.syncapp.R;
 
 /**
@@ -14,6 +17,12 @@ import com.sync.syncapp.R;
  * create an instance of this fragment.
  */
 public class StatisticsFragment extends Fragment {
+    
+    CheckBox toggleCo2;
+    CheckBox toggleTemp;
+    CheckBox toggleLight;
+    
+    LineChart chart;
 
     /**
      * Use this factory method to create a new instance of
@@ -30,17 +39,24 @@ public class StatisticsFragment extends Fragment {
         
         
     }
+    
+    public void onStart() {
+        super.onStart();
+        
+        Activity a = getActivity();
+        toggleCo2 = (CheckBox) a.findViewById(R.id.stats_toggle_co2);
+        toggleTemp = (CheckBox) a.findViewById(R.id.stats_toggle_temp);
+        toggleLight = (CheckBox) a.findViewById(R.id.stats_toggle_light);
+        
+        chart = (LineChart) a.findViewById(R.id.stats_chart);
+        
+        //TODO: get statistics and set up the line chart
+    }
 
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_statistics, container, false);
-    }
-
-    public void onStart() {
-        super.onStart();
-
-        //TODO: get statistics data and set variables
     }
 }
